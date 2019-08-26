44d1a4b204391c58b3f519ae89ad40694e7d4817
“liyijun123 <2510243137@qq.com>
2019-08-26 23:28:58 +0800

    youxi

--- src/main/java/tw/Main.java
+++ src/main/java/tw/Main.java
@@ -1,5 +1,10 @@
 package tw;
 
+import javax.print.attribute.standard.OutputDeviceAssigned;
+
+import org.omg.CORBA.PUBLIC_MEMBER;
+import org.omg.PortableServer.ID_ASSIGNMENT_POLICY_ID;
+
 import tw.commands.GuessInputCommand;
 
 /**
@@ -8,11 +13,22 @@ import tw.commands.GuessInputCommand;
 public class Main {
 
     private static final int RUN_TIMES = 6;
-
+    static String str="1234";
     public static void main(String[] args) throws Exception {
-        for (int i = 0; i < RUN_TIMES; i++) {
-            String input = new GuessInputCommand().input();
-            System.out.println(input);
+        String result = "";
+     for (int i = 0; i < RUN_TIMES; i++) {
+            String input = new GuessInputCommand().input();   
+            result=output(input,str);
+            System.out.println(result);
+        }     
+         
+//            System.out.println(input);
         }
-    }
+    
+    
+ public static String output(String input,String str) {
+  if(input.equals(str)) {
+   return "right";
+  }
+
 }
